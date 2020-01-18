23b8d3a79adecbaba1d732e344aa6863fcb07f2f
Pedro Marques <pmark.pt@gmail.com>
2020-01-18 20:22:27 +0000

    implemented dynamic list

--- cillionaire.c
+++ cillionaire.c
@@ -6,31 +6,58 @@
 #include <time.h>
 
 #define MSG_UNKNOWN "Unknown option."
+#define MSG_BYE "*** Sad to see you go..."
+#define MSG_NOQUESTIONS "*** This is embarrassing but we're out of questions."
 #define MSG_NOFILE "No file provided. Please start the program with the argument -f [file name]."
-#define MAX_QUESTIONS 750
 
 enum category_enum {ent_books, ent_film, ent_music, ent_musicals, ent_tv, ent_vgames, ent_bgames, ent_comics, ent_anime, ent_cartoons, gen_knowledge, science_nature,
 science_gadgets, science_computers, science_math, mythology, sports, geography, history, politics, art, celebrities, animals, vehicles};
 
 enum difficulty_enum {easy, medium, hard};
 
-struct game_question
+typedef struct
 {
     char question[128];
     char answers[4][64];
     enum category_enum ctg;
     enum difficulty_enum diff;
-};
+} game_question;
+
+typedef struct _node {
+    game_question q;
+    struct _node * next;
+    struct _node * previous;
+} node;
+
+typedef struct _node * link;
+
+typedef struct {
+    char name[50];
+    bool j50, j25;
+    short level_index;
+    bool last_answer;
+} Player;
 
 void print_menu(void);
 int rand_number(void);
-void read_questions_file(char *, struct game_question *);
+void read_questions_file(char *, link *);
+bool play(Player *, link *, int *, short *);
+int add_question(link *, game_question, int);
+game_question get_question(link *, enum difficulty_enum);
+int rand_number(void);
+void start_new_game(Player *);
+char show_question(game_question *);
 
 int main(int agrc, char **argv)
 {
     char user_input, file_name[100];
-    bool player_seed = false, file_provided = false, active = true;
-    struct game_question questions[MAX_QUESTIONS];
+    bool player_seed = false, file_provided = false, active = true, playing = false;
+    int levels[] = {0, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000};
+    short difficulty_level[] = {0, 0, 0, 1, 1, 2, 2, 2};
+    Player current_player;
+    link questions_link;
+    game_question current_question;
+    char current_question_answer;
 
     if(agrc > 1)
     {
@@ -64,20 +91,8 @@ int main(int agrc, char **argv)
     }
     else
     {
-        read_questions_file(file_name, questions);
-        for (unsigned short i = 0; i < 5; i++)
-        {
-            printf("Question %d: %s\n", i, questions[i].question);
-            for (unsigned short j = 0; j < 4; j++)
-            {
-                printf("Option %d: %s\n", j, questions[i].answers[j]);
-            }
-            printf("Category enum: %d\n", questions[i].ctg);
-            printf("Category diff: %d\n", questions[i].diff);
-        }
-        return 0;
+        read_questions_file(file_name, &questions_link);
     }
-
 
     print_menu();
     while (active)
@@ -90,10 +105,27 @@ int main(int agrc, char **argv)
         switch (user_input)
         {
             case 'q':
+                puts(MSG_BYE);
                 active = false;
                 break;
             case 'h':
                 print_menu();
+                break;
+            case 'n':
+                start_new_game(&current_player);
+                current_question = get_question(&questions_link, difficulty_level[current_player.level_index]);
+                current_question_answer = show_question(&current_question);
+                playing = true;
+                break;
+            case 'A':
+            case 'B':
+            case 'C':
+            case 'D':
+                if (!playing)
+                    puts(MSG_UNKNOWN);
+                else
+
+                
                 break;
             default:
                 puts(MSG_UNKNOWN);
@@ -103,45 +135,38 @@ int main(int agrc, char **argv)
 
 }
 
-void trim_string(char * s1)
+void trim_new_line(char * s1)
 {
 	const unsigned short length = strlen(s1);
-	char a;
-	bool end = false;
 
 	for (unsigned short i = 0; i < length; i++)
 	{
-		a = *(s1 + i);
-
-		if (a == '\n')
-		{
-			end = true;
-		}
-
-		if (end)
+		if (*(s1 + i) == '\n')
 		{
 			*(s1 + i) = '\0';
 		}
 	}
 }
 
-bool strings_are_equal(char * s1, char * s2)
+void trim_leading_white_space(char * s)
 {
-	const unsigned short length = strlen(s1);
-	char a, b;
+    const unsigned short length = strlen(s);
+    unsigned short i = 0, k = 0;
 
-	for (unsigned short i = 0; i < length; i++)
-	{
-		a = *(s1 + i);
-		b = *(s2 + i);
-
-		if (a != b)
-		{
-			return false;
-		}
-	}
-
-	return true;
+    for (i = 0; i < length; i++)
+    {
+        if (s[i] != 32)
+        {
+            k = i;
+            break;
+        }
+    }
+    
+    for (i = 0; i < length; i++)
+    {
+        s[i] = s[k];
+        k++;
+    }
 }
 
 enum difficulty_enum get_difficulty_enum_from_string(char * s)
@@ -151,7 +176,7 @@ enum difficulty_enum get_difficulty_enum_from_string(char * s)
 
     for (i = 0; i < 3; i++)
     {
-        if (strings_are_equal(s, diff_literal[i]))
+        if (strcmp(s, diff_literal[i]) == 0)
         {
           	return i;
         }
@@ -170,7 +195,7 @@ enum category_enum get_category_enum_from_string(char * s)
 
     for (i = 0; i < 24; i++)
     {
-        if (strings_are_equal(s, categories_literal[i]))
+        if (strcmp(s, categories_literal[i]) == 0)
         {
           return i;
         }
@@ -185,21 +210,20 @@ void set_value_to_string_after_equal(char * phrase, char * value)
 	memset(value, 0, strlen(value));
 	for (unsigned short i = 0, j = 0; i < strlen(phrase); i++)
     {
-        if (!found_equal && *(phrase + i) == '=')
-        {
-            found_equal = true;
-            continue;
-        }
-
         if (found_equal)
         {
             *(value + j) = *(phrase + i);
             j++;
         }
+        else if (*(phrase + i) == '=')
+        {
+            found_equal = true;
+            continue;
+        }
     }
 }
 
-bool string_start_compare(char * string, char * value)
+bool string_starts_with(char * string, char * value)
 {
     for (unsigned short i = 0; i < strlen(value); i++)
     {
@@ -210,11 +234,45 @@ bool string_start_compare(char * string, char * value)
     return true;
 }
 
-void read_questions_file(char * file_name, struct game_question * q)
+void write_question(FILE * file, link * last_question, char * question_line, int question_count)
 {
-	unsigned short option_index = 0, question_count = 0;
+    char line[256], tmp[128];
+    game_question working_question;
+
+    trim_new_line(question_line);
+    set_value_to_string_after_equal(question_line, working_question.question);
+
+    for (unsigned short i = 0; i < 4; i++)
+    {
+        if(fgets(line, 256, file) != NULL)
+        {
+            trim_new_line(line);
+            set_value_to_string_after_equal(line, working_question.answers[i]);
+        }
+    }
+    
+    if(fgets(line, 256, file) != NULL)
+    {
+        trim_new_line(line);
+        set_value_to_string_after_equal(line, tmp);
+        working_question.ctg = get_category_enum_from_string(tmp);
+    }
+
+    if(fgets(line, 256, file) != NULL)
+    {
+        trim_new_line(line);
+        set_value_to_string_after_equal(line, tmp);
+        working_question.diff = get_difficulty_enum_from_string(tmp);
+    }
+    
+    add_question(last_question, working_question, question_count);
+}
+
+void read_questions_file(char * file_name, link * questions_link)
+{
+    unsigned int question_count = 0;
 	char line[256];
-	bool finished_question = false;
+    link last_question;
 	FILE * f = fopen(file_name, "r");
 
 	if (f == NULL)
@@ -225,51 +283,85 @@ void read_questions_file(char * file_name, struct game_question * q)
 
 	while (!feof(f))
 	{
-		struct game_question question;
-
-		if (finished_question)
-		{
-			*(q + question_count) = question;
-			question_count++;
-			finished_question = false;
-		}
-
 		if (fgets(line, 256, f) != NULL)
 		{
-			if (string_start_compare(line, ";"))
+			if (string_starts_with(line, ";"))
 			{
 				continue;
 			}
-
-			trim_string(line);
-			if (string_start_compare(line, "QUESTION"))
-			{
-				set_value_to_string_after_equal(line, question.question);
-			}
-			else if (string_start_compare(line, "OPTION"))
-			{
-				set_value_to_string_after_equal(line, question.answers[option_index]);
-				option_index++;
-				if (option_index == 4)
-					option_index = 0;
-			}
-			else if (string_start_compare(line, "CATEGORY"))
-			{
-				char literal_value[128];
-				set_value_to_string_after_equal(line, literal_value);
-				question.ctg = get_category_enum_from_string(literal_value);
-			}
-			else if (string_start_compare(line, "DIFFICULTY"))
-			{
-				char literal_value[128];
-				set_value_to_string_after_equal(line, literal_value);
-				question.diff = get_difficulty_enum_from_string(literal_value);
-				finished_question = true;
-			}
+            else if (string_starts_with(line, "QUESTION"))
+            {
+                write_question(f, &last_question, line, question_count);
+                if (question_count == 0)
+                {
+                    *questions_link = last_question;
+                }
+                question_count++;
+            }
 		}
 	}
 
 	fclose(f);
+}
+
+void start_new_game(Player * p)
+{
+    char default_name[] = "newbie";
+
+    fgets(p->name, 50, stdin);
+    trim_new_line(p->name);
+    if(strlen(&(*p->name)) == 0)
+    {
+        for (unsigned short i = 0; i < strlen(default_name); i++)
+            p->name[i] = default_name[i];
+    }
+    else
+        trim_leading_white_space(p->name);
+
+    printf("*** Hi %s, let's get started!\n", p->name);
+}
+
+void move_correct_answer(game_question * q, int correct_answer_index)
+{
+    char tmp[64];
+    
+    strcpy(tmp, q->answers[0]);
+    for (short i = 0; i < correct_answer_index; i++)
+    {
+        strcpy(q->answers[i], q->answers[i + 1]);
+    }
+
+    strcpy(q->answers[correct_answer_index], tmp);
+}
+
+char show_question(game_question * q)
+{
+    int correct_answer_index = rand_number();
+    move_correct_answer(q, correct_answer_index);
+
+    printf("*** Question: %s\n", q->question);
+
+    for (short i = 0, k = 65; i < 4; i++, k++)
+    {
+        printf("*** %c: %s\n", (char)k, q->answers[i]);
+    }
+
+    return (char)(65 + correct_answer_index);
+}
+
+void handle_player_question_response(Player * p, char * user_answer, char correct_answer)
+{
+    if (user_answer == correct_answer)
+    {
+        p->level_index++;
+        p->last_answer = true;
+    }
+    else
+    {
+        p->level_index = p->level_index > 0 ? p->level_index - 1 : 0;
+        
+        if (p->)
+    }
 }
 
 int rand_number(void)
@@ -291,4 +383,47 @@ void print_menu(void)
     puts("*** j 25         - play 25:75 joker        *");
     puts("*** c            - show credits            *");
     puts("********************************************");
+}
+
+int add_question(link * last_question, game_question q, int question_count)
+{
+    link tmp = (node *)malloc(sizeof(node));
+    if (tmp == NULL)
+    {
+        puts("Out of memory!");
+        return 0;
+    }
+
+    tmp->q = q;
+    tmp->next = NULL;
+    tmp->previous = NULL;
+
+    if (question_count > 0)
+    {
+        tmp->previous = *last_question;
+        (*last_question)->next = tmp;
+    }
+
+    *last_question = tmp;
+    return 1;
+}
+
+game_question get_question(link * questions, enum difficulty_enum diff)
+{
+    game_question x;
+
+    for (link aux = *questions; aux != NULL; aux = aux->next)
+    {
+        if (aux->q.diff == diff)
+        {
+            x = aux->q;
+            aux->previous->next = aux->next;
+
+            free(aux);
+            return x;
+        }
+    }
+
+    puts(MSG_NOQUESTIONS);
+    exit(0);
 }
